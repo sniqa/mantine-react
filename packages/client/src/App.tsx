30d32cd21cccb5a3b26cb973a941b41ffc8ea4dc
sniqa <sniqasystem@outlook.com>
2023-02-08 23:49:29 +0800

    02/08/2

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useToggleDarkModel } from '@libs/toggleDarkModel'
 import { ColorSchemeProvider, MantineProvider } from '@mantine/core'
 import { NotificationsProvider } from '@mantine/notifications'
+import { NavigationProgress } from '@mantine/nprogress'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 
@@ -18,6 +19,7 @@ export default () => {
 				withNormalizeCSS
 			>
 				<NotificationsProvider position="top-center">
+					<NavigationProgress />
 					<RouterProvider router={router} />
 				</NotificationsProvider>
 			</MantineProvider>
